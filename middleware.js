512f4b322f5ed1fdad52271b3c4756336f055e62
Anushka Jana <your-email@example.com>
2026-03-06 02:40:24 +0530

    Fix hero image path (banner.png) and update hero section

--- middleware.js
+++ middleware.js
@@ -10,13 +10,12 @@ export default clerkMiddleware(async (auth, req) => {
   const { userId, redirectToSignIn } = await auth();
 
   if (!userId && isProtectedRoute(req)) {
-    return redirectToSignIn();
+    return redirectToSignIn({
+      returnBackUrl: req.url,
+    });
   }
 });
 
 export const config = {
-  matcher: [
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    "/(api|trpc)(.*)",
-  ],
+  matcher: ["/((?!_next|.*\\..*).*)"],
 };
